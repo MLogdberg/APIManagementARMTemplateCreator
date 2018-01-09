aa1dddcd40a738619844cc91880e8521eb729811
Martin Peters <m.peters@agrifirm.com>
2018-01-09 14:14:43 +0100

    Token parameter did not work.

--- APIManagementTemplate/GeneratorCmdlet.cs
+++ APIManagementTemplate/GeneratorCmdlet.cs
@@ -18,75 +18,74 @@ namespace APIManagementTemplate
             HelpMessage = "Name of the API Management instance"
             )]
         public string APIManagement;
-        [Parameter(
-            Mandatory = true,
-            HelpMessage = "Name of the Resource Group"
-            )]
-        public string ResourceGroup;
+
         [Parameter(
             Mandatory = true,
             HelpMessage = "The name of the Resource Group"
             )]
-        public string SubscriptionId;
+        public string ResourceGroup;
+
         [Parameter(
             Mandatory = false,
             HelpMessage = "The Subscription id (guid)"
             )]
-        public string TenantName = "";
+        public string SubscriptionId;
+
         [Parameter(
             Mandatory = false,
             HelpMessage = "Name of the Tenant i.e. contoso.onmicrosoft.com"
         )]
+        public string TenantName = "";
 
         //see filter in https://docs.microsoft.com/en-us/rest/api/apimanagement/api/listbyservice
-        public string APIFilters = null;
         [Parameter(
             Mandatory = false,
             HelpMessage = "Filter for what API's to exort i.e: path eq 'api/v1/currencyconverter' or endswith(path,'currencyconverter')",
             ValueFromPipeline = true
         )]
+        public string APIFilters = null;
 
-        public bool ExportAuthorizationServers = true;
         [Parameter(
             Mandatory = false,
             HelpMessage = "Export AuthorizationServers",
             ValueFromPipeline = true
         )]
+        public bool ExportAuthorizationServers = true;
 
-        public bool ExportPIManagementInstance = true;
         [Parameter(
             Mandatory = false,
             HelpMessage = "Export the API Management Instance",
             ValueFromPipeline = true
         )]
+        public bool ExportPIManagementInstance = true;
 
-        public bool ExportGroups = true;
         [Parameter(
             Mandatory = false,
             HelpMessage = "Export the API Management Groups, not builtin",
             ValueFromPipeline = true
         )]
+        public bool ExportGroups = true;
 
-        public bool ExportProducts = true;
         [Parameter(
             Mandatory = false,
             HelpMessage = "Export the API Management Products",
             ValueFromPipeline = true
         )]
+        public bool ExportProducts = true;
 
-        public string Token = "";
         [Parameter(
             Mandatory = false,
             HelpMessage = "Piped input from armclient",
             ValueFromPipeline = true
         )]
+        public string Token = "";
 
-        public bool ParametrizePropertiesOnly = false;
         [Parameter(
             Mandatory = false,
             HelpMessage = "Set to 'true' when all environment-specific parameters are defined as properties",
             ValueFromPipeline = true
         )]
+        public bool ParametrizePropertiesOnly = false;
 
         public string ClaimsDump;
 
@@ -98,11 +97,7 @@ namespace APIManagementTemplate
 
                 if (ClaimsDump == null)
                 {
-                    if (String.IsNullOrEmpty(Token))
-                    {
-                        Token = resourceCollector.Login(TenantName);
-                        //WriteVerbose(Token);
-                    }
+                    resourceCollector.token = String.IsNullOrEmpty(Token) ? resourceCollector.Login(TenantName) : Token;
                 }
                 else if (ClaimsDump.Contains("Token copied"))
                 {
