7bbc1e31ab9dff7291c6163bda3514bf8eb5ac9a
Gert Vloo <gertvloo@hotmail.com>
2019-03-06 12:20:36 +0100

    Property SubscriptionRequired not supported before 208-06-01-preview. So generated property will be removed now.

--- APIManagementTemplate/Models/DeploymentTemplate.cs
+++ APIManagementTemplate/Models/DeploymentTemplate.cs
@@ -274,6 +274,11 @@ namespace APIManagementTemplate.Models
             AddParameterFromObject((JObject)resource["properties"], "serviceUrl", "string", name);
             AddParameterFromObject((JObject)resource["properties"], "apiVersion", "string", name);
             AddParameterFromObject((JObject)resource["properties"], "isCurrent", "bool", name);
+                       
+            if (DateTime.Parse(obj.apiVersion.Replace("-preview","")) < DateTime.Parse("2018-06-01"))
+            {
+                ((JObject)resource["properties"]).Remove("subscriptionRequired");
+            }
 
             if (APIMInstanceAdded)
             {
