8f6fa15c375e3517aa77b381252b0a8f85b2d892
Mattias <m.logdberg@gmail.com>
2020-05-08 08:45:19 +0200

    updated auth url since the breaking change v3->v4

--- APIManagementTemplate/Constants/Constants.cs
+++ APIManagementTemplate/Constants/Constants.cs
@@ -12,7 +12,7 @@ namespace APIManagementTemplate
         internal static readonly string deploymenParameterSchema = @"https://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#";
         internal static readonly string parameterSchema = @"https://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#";
 
-        public static string AuthString = "https://login.windows.net/common/oauth2/authorize";
+        public static string AuthString = "https://login.microsoftonline.com/common"; //"https://login.windows.net/common/oauth2/authorize";
         public static string ClientId = "1950a258-227b-4e31-a9cf-717495945fc2";
         public static string ResourceUrl = "https://management.core.windows.net/";
         public static string RedirectUrl = "urn:ietf:wg:oauth:2.0:oob";
